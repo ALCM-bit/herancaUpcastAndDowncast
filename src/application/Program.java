3dbb854af0c12f4b5351fdbe5e80dab900c3defc
Felipe Augusto <felipealcmagalhaes@gmail.com>
2021-07-20 13:40:28 -0300

    Add Logica de saque usando sobreposisão, super e Override

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account(1001, "Alex", 0.0);
+		Account acc = new Account(1001, "Alex", 1000.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
 		// UPCASTING
@@ -17,7 +17,7 @@ public class Program {
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
 		
-		// DOWCASTING
+		// DOWNCASTING
 		
 		BusinessAccount acc4 = (BusinessAccount)acc2;
 		acc4.loan(100.0);
@@ -34,6 +34,20 @@ public class Program {
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
+		
+		Account acc6 = new Account(1005, "Alex", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		//UTILIZANDO SOBREPOSIÇÃO
+		Account acc7 = new SavingsAccount(1006, "Maria", 1000.0, 0.01);
+		acc7.withdraw(200.00);
+		System.out.println(acc7.getBalance());
+		
+		// UTILIZANDO ANOTATION E SUPER
+		Account acc8 = new BusinessAccount(1007, "Bob", 1000.0, 500.00);
+		acc8.withdraw(200.00);
+		System.out.println(acc8.getBalance());
 	}
 
 }
